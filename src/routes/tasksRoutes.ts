059e6f17be5a11e6af350f473b2c44c8507f74a0
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-01-23 12:03:42 -0300

    feat: create routes file

--- src/routes/tasksRoutes.ts
+++ src/routes/tasksRoutes.ts
@@ -1,1 +1,10 @@
+import { Router } from "express";
 
+const router = Router();
+
+router.post("/tasks");
+router.get("/tasks");
+router.delete("/tasks");
+router.put("/tasks");
+
+export default router;
