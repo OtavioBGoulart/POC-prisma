9c656c47cd0037ec9e9630148dafb9a685cb9a41
Otavio Goulart <otaviobgoulartt@gmail.com>
2023-01-26 10:09:07 -0300

    feat: users schemas

---
+++ src/schemas/usersSechemas.ts
@@ -0,0 +1,15 @@
+import joi from 'joi';
+
+export const signinSchema = joi.object({
+  email: joi.string().email().required(),
+  password: joi.string().required(),
+});
+
+
+export const signupSchema = joi.object({
+    name: joi.string().alphanum().max(15).min(3).required(),
+    email: joi.string().email().required(),
+    password: joi.string().min(6).required(),
+    confirmPassword: joi.string().required()
+  });
+  
